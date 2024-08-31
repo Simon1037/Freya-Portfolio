00f2b89da3416ed55d8d25d5d3ffe2051b27b7d6
freyajoy <freyajoyhaycock@gmail.com>
2024-08-31 20:21:04 +0200

    farve hover på li på nav

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -2,3 +2,14 @@ console.log('Debug master')
 document.getElementById('menuToggle').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('menu-open');
 });
+
+
+document.addEventListener('click', function (event) {
+    var menu = document.getElementById('menu');
+    var toggleButton = document.getElementById('menuToggle');
+
+  
+    if (!menu.contains(event.target) && !toggleButton.contains(event.target)) {
+        menu.classList.remove('menu-open');
+    }
+});
